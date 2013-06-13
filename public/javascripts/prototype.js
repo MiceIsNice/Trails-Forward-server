f804c7873e3b4e7991445dfad55bbaa05d45a071
AaronTufts <aarontietz@gmail.com>
2013-06-13 16:53:50 -0400

    added cross domain permissions

--- public/javascripts/prototype.js
+++ public/javascripts/prototype.js
@@ -1538,7 +1538,8 @@ Ajax.Request = Class.create(Ajax.Base, {
     var headers = {
       'X-Requested-With': 'XMLHttpRequest',
       'X-Prototype-Version': Prototype.Version,
-      'Accept': 'text/javascript, text/html, application/xml, text/xml, */*'
+      'Accept': 'text/javascript, text/html, application/xml, text/xml, */*',
+      'Access-Control-Allow-Origin' : '*'
     };
 
     if (this.method == 'post') {
