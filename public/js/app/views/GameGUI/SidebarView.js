99f8c70b9f420d2a4cccdd57b5216b48501a4afa
ME4T <ME4T@wr-130-64-194-55.medford.tufts.edu>
2013-11-07 11:57:45 -0500

    commented out update contracts temporarily"

--- public/js/app/views/GameGUI/SidebarView.js
+++ public/js/app/views/GameGUI/SidebarView.js
@@ -31,28 +31,28 @@ TFApp.SidebarView = Backbone.View.extend({
 		console.log("Starting Sidebar View");
 	},
 	updateContracts: function(){
-		var contracts = TFApp.models.currentWorldModel.get("contractCollection");
-		console.log("updateContracts", contracts);
-		this.$contracts.find("table").empty();
-		this.$myContracts.find("table").empty();
-		for(var i = 0;i<contracts.models.length;i++){
+		// var contracts = TFApp.models.currentWorldModel.get("contractCollection");
+		// console.log("updateContracts", contracts);
+		// this.$contracts.find("table").empty();
+		// this.$myContracts.find("table").empty();
+		// for(var i = 0;i<contracts.models.length;i++){
 
-			var contract = contracts.models[i].get("contract");
-			if(contract.player_id == TFApp.models.currentPlayerModel.get("player_id")){
+		// 	var contract = contracts.models[i].get("contract");
+		// 	if(contract.player_id == TFApp.models.currentPlayerModel.get("player_id")){
 
-			}else{
-				var $tr = $('<tr class="contract white-bar"></tr>');
-				var $nameTd = $('<td class="contract-name">'+contract.id+'</td>');
-				var $lumberTd = $('<td class="lumber lumber-icon">'+contract.id+'</td>');
-				// <tr class="contract white-bar hoverable" data-contract-id="1">
-				// 	<td class="contract-name">Contract 01</td>
-				// 	<td class="lumber lumber-icon">400</td>
-				// 	<td class="money currency-icon">2000</td>
-				// </tr>
-			}
-			console.log(contract);
+		// 	}else{
+		// 		var $tr = $('<tr class="contract white-bar"></tr>');
+		// 		var $nameTd = $('<td class="contract-name">'+contract.id+'</td>');
+		// 		var $lumberTd = $('<td class="lumber lumber-icon">'+contract.id+'</td>');
+		// 		// <tr class="contract white-bar hoverable" data-contract-id="1">
+		// 		// 	<td class="contract-name">Contract 01</td>
+		// 		// 	<td class="lumber lumber-icon">400</td>
+		// 		// 	<td class="money currency-icon">2000</td>
+		// 		// </tr>
+		// 	}
+		// 	console.log(contract);
 
-		}
+		// }
 	},
 	updatePlayerValues: function(){
 
