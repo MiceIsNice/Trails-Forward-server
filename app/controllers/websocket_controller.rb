a97ce5fe7025fe6cdb81a1cd68eb69883eb28110
ME4T <ME4T@wr-130-64-194-86.medford.tufts.edu>
2013-12-04 14:52:52 -0500

    continued improvements to the websocket tests

--- app/controllers/websocket_controller.rb
+++ app/controllers/websocket_controller.rb
@@ -8,22 +8,13 @@ class WebsocketController < ApplicationController
 
 
   def index
-
     conn = Bunny.new(:host => "127.0.0.1")
     conn.start
     ch = conn.create_channel
     x = ch.fanout("worldchat3");
-
-    x.publish("Sending a message to worldchat3")
-
-    # ch.default_exchange.publish("HELLO WORLD!", :routing_key => q.name)
-    # #q.publish("ANYTHING", :routing_key => q.name)
-    # puts "[x] Send 'Hello World!'"
-
+    x.publish(params[:text]);
     conn.close
-
     render json: {:message => ""}
-
   end
 
 
